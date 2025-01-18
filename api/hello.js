c083f2e8bc8f51b8c66134c337c60c405370bfa0
j6ohn <chongyetyuenone@gmail.com>
2025-01-18 12:03:59 +0800

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello" });
+  try {
+    res.status(200).json({ message: "Hello" });
+  } catch (error) {
+    res.status(500).json({ error: "An internal server error occurred." });
+  }
 }
